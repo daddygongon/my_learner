20e1b38561d5881ead77abf0e27f3bfcfe039929
Shigeto R. Nishitani <daddygongon@users.noreply.github.com>
2021-07-25 11:58:25 +0900

    first commit

---
+++ lib/my_learner.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require_relative "my_learner/version"
+
+module MyLearner
+  class Error < StandardError; end
+  # Your code goes here...
+
+  puts "hello"
+  
+end
